7280fb4dd76a27f8109c591368a735fed76974a1
Poempoat <97211503+Poempoat@users.noreply.github.com>
2022-09-27 21:20:54 +0800

    Add files via upload

---
+++ unknown/unknown/Z01/AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Z01.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Z01-user \
+    omni_Z01-userdebug \
+    omni_Z01-eng
---
+++ unknown/unknown/Z01/omni_Z01.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from Z01 device
+$(call inherit-product, device/unknown/Z01/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Z01
+PRODUCT_NAME := omni_Z01
+PRODUCT_BRAND := qcom
+PRODUCT_MODEL := V20211220
+PRODUCT_MANUFACTURER := unknown
